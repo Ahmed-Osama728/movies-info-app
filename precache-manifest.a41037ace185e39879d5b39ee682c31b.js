557348b5d7fe858ff78f68cab98d874a7ab5b871
Ahmed-Osama728 <https://github.com/Ahmed-Osama728/movies-info-app>
2020-09-25 23:05:31 +0200

    Updates

---
+++ precache-manifest.a41037ace185e39879d5b39ee682c31b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48628d458469d3844472c8c56997b3bd",
+    "url": "/movies-info-app/index.html"
+  },
+  {
+    "revision": "8cf453790654a711d354",
+    "url": "/movies-info-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "59ff0a9a362369528ee6",
+    "url": "/movies-info-app/static/css/main.c0fa3f04.chunk.css"
+  },
+  {
+    "revision": "8cf453790654a711d354",
+    "url": "/movies-info-app/static/js/2.0d6111d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-info-app/static/js/2.0d6111d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ff0a9a362369528ee6",
+    "url": "/movies-info-app/static/js/main.abe59aa6.chunk.js"
+  },
+  {
+    "revision": "26284eab49f795b8fa07",
+    "url": "/movies-info-app/static/js/runtime-main.42b9dd11.js"
+  }
+]);
